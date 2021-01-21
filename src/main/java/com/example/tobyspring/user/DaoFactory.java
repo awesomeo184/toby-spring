51a506f04925be4443c081b3e1d13cb54a6ca23e
awesomeo184 <awesomeo184@gmail.com>
2021-01-21 17:29:18 +0900

    1.4.2 오브젝트 팩토리의 활용

--- src/main/java/com/example/tobyspring/user/DaoFactory.java
+++ src/main/java/com/example/tobyspring/user/DaoFactory.java
@@ -1,14 +1,28 @@
 package com.example.tobyspring.user;
 
+import com.example.tobyspring.user.dao.AccountDao;
 import com.example.tobyspring.user.dao.ConnectionMaker;
+import com.example.tobyspring.user.dao.DConnectionMaker;
+import com.example.tobyspring.user.dao.MessageDao;
 import com.example.tobyspring.user.dao.NConnectionMaker;
 import com.example.tobyspring.user.dao.UserDao;
 
 public class DaoFactory {
 
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new NConnectionMaker();
-        return new UserDao(connectionMaker);
+        return new UserDao(connectionMaker());
+    }
+
+    public AccountDao accountDao() {
+        return new AccountDao(connectionMaker());
+    }
+
+    public MessageDao messageDao() {
+        return new MessageDao(connectionMaker());
+    }
+
+    public ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 
 }

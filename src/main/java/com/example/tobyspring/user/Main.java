c82238377fc0f85d2104e53c247a6ccd119ae889
awesomeo184 <awesomeo184@gmail.com>
2021-01-21 15:00:12 +0900

    1.1 초난감 dao

---
+++ src/main/java/com/example/tobyspring/user/Main.java
@@ -0,0 +1,28 @@
+package com.example.tobyspring.user;
+
+import com.example.tobyspring.user.dao.UserDao;
+import com.example.tobyspring.user.domain.User;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("spring");
+        user.setName("정수현");
+        user.setPassword("hello");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = dao.get(user.getId());
+        System.out.println(user2.getId());
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+}

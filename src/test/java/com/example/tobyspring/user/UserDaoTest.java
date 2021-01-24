2eb5e908d21184d8a50db18353024c7daaac854d
awesomeo184 <awesomeo184@gmail.com>
2021-01-25 00:45:18 +0900

    2.3.3 포괄적인 테스트. 실패하는 테스트 작성
    - 항상 네거티브 테스트를 먼저 만들라

--- src/test/java/com/example/tobyspring/user/UserDaoTest.java
+++ src/test/java/com/example/tobyspring/user/UserDaoTest.java
@@ -9,6 +9,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
 
 public class UserDaoTest {
 
@@ -59,6 +60,19 @@ public class UserDaoTest {
         userDao.add(user3);
         assertThat(userDao.getCount()).isEqualTo(3);
 
+    }
+
+    @Test
+    void getUserFailure() throws SQLException, ClassNotFoundException {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
+
+        UserDao userDao = ac.getBean("userDao", UserDao.class);
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
+
+        assertThatThrownBy(() -> {
+            userDao.get("unknownId");
+        }).isInstanceOf(EmptyResultDataAccessException.class);
     }
 
 }

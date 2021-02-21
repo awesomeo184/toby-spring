4c0fa654f6278912984a8982e3539235f8e4b414
awesomeo184 <awesomeo184@gmail.com>
2021-02-22 01:13:05 +0900

    chapter2 init

---
+++ src/main/java/chapter1/unit2/UserDaoTest.java
@@ -0,0 +1,23 @@
+package chapter1.unit2;
+
+import java.sql.SQLException;
+
+public class UserDaoTest {
+
+    public static void main(String[] args) throws SQLException {
+        UserDao dao = new UserDao();
+
+        User user = new User();
+        user.setId("whiteship");
+        user.setName("백기선");
+        user.setPassword("married");
+
+        dao.add(user);
+
+        User findUser = dao.get(user.getId());
+
+        System.out.println("name = " + findUser.getName());
+        System.out.println("password = " + findUser.getPassword());
+    }
+
+}

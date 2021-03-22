2b847dbde0c40810a73a857f9bfeea52629aa24d
awesomeo184 <awesomeo184@gmail.com>
2021-03-22 17:52:53 +0900

    chapter3.unit3 init

---
+++ src/main/java/chapter3/unit3/DeleteAllStatement.java
@@ -0,0 +1,14 @@
+package chapter3.unit3;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DeleteAllStatement implements StatementStrategy {
+
+    @Override
+    public PreparedStatement makePreparedStatement(Connection con) throws SQLException {
+        PreparedStatement ps = con.prepareStatement("delete from users");
+        return ps;
+    }
+}
---
+++ src/main/java/chapter3/unit3/UserDao.java
@@ -0,0 +1,131 @@
+package chapter3.unit3;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+public class UserDao {
+
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void add(User user) throws SQLException {
+        Connection con = dataSource.getConnection();
+
+        PreparedStatement ps = con
+            .prepareStatement("insert into users (id, name, password) values (?,?,?)");
+
+        ps.setString(1, user.getId());
+        ps.setString(2, user.getName());
+        ps.setString(3, user.getPassword());
+
+        ps.executeUpdate();
+
+        ps.close();
+        con.close();
+    }
+
+    public User get(String id) throws SQLException {
+        Connection con = dataSource.getConnection();
+
+        PreparedStatement ps = con
+            .prepareStatement("select * from users where id = ?");
+
+        ps.setString(1, id);
+
+        ResultSet rs = ps.executeQuery();
+
+        User user = null;
+        if(rs.next()) {
+            user = new User();
+            user.setId(rs.getString("id"));
+            user.setName(rs.getString("name"));
+            user.setPassword(rs.getString("password"));
+        }
+        rs.close();
+        ps.close();
+        con.close();
+
+        if (user == null) {
+            throw new EmptyResultDataAccessException(1);
+        }
+
+        return user;
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try {
+            con = dataSource.getConnection();
+
+            ps = stmt.makePreparedStatement(con);
+
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            }
+
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {
+                }
+            }
+        }
+    }
+
+    public void deleteAll() throws SQLException {
+        StatementStrategy stmt = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(stmt);
+    }
+
+
+    public int getCount() throws SQLException {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            con = dataSource.getConnection();
+            ps = con.prepareStatement("select count(*) from users");
+
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {}
+            }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {}
+            }
+            if (con != null) {
+                try {
+                    con.close();
+                } catch (SQLException e) {}
+            }
+        }
+    }
+
+}
